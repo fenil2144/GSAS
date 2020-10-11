c0d1c5ad66e974b2a620092d0b306c602b0794da
DiviyanshaAgarwal <71273582+DiviyanshaAgarwal@users.noreply.github.com>
2020-10-11 22:22:26 +0530

    Latest commit

--- src/com/gsas/service/SchemeServiceImpl.java
+++ src/com/gsas/service/SchemeServiceImpl.java
@@ -187,13 +187,13 @@ public class SchemeServiceImpl implements SchemeService {
 	}
 
 	@Override
-	public List<DocumentVO> getSchemeDocumentsList(Long scheme_id) throws DatabaseException {
-		return schemeDao.getSchemeDocumentsList(scheme_id);
+	public List<DocumentVO> getSchemeDocumentsList(Long schemeId) throws DatabaseException {
+		return schemeDao.getSchemeDocumentsList(schemeId);
 	}
 
 	@Override
-	public List<BankVO> getSchemeBankList(Long scheme_id) throws DatabaseException {
-		return schemeDao.getSchemeBankList(scheme_id);
+	public List<BankVO> getSchemeBankList(Long schemeId) throws DatabaseException {
+		return schemeDao.getSchemeBankList(schemeId);
 	}
 
 }
